007b196bc70ea25e2072260a403a1ddf4f4e4151
Justen Betcher <betcherj1@newpaltz.edu>
2023-03-02 15:22:27 -0500

    end of class

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import Products from '../views/Products.vue'
+import Login from '../views/Login.vue'
+import { useSessoin } from '../model/session'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,8 +21,29 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       //component: () => import('../views/AboutView.vue')
       component: AboutView
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: Products,
+      beforeEnter:
+    }, 
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
 
 export default router
+
+function secureRoute (to, from , next) {
+  const session = useSession();
+  if (session.user) {
+    next() 
+  } 
+  else { 
+    next('/login')
+  }
+}
---
+++ client/src/views/Products.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1 class="title">
+        These will be the products
+    </h1>
+    <h2 class="subtitile">
+        If you got here then you are logged in
+    </h2>
+</template>
+
+
+
+<style scoped>
+
+</style>
