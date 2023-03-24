9af57ca919e06978ef5c445f5efecca3accb2d05
Justen Betcher <betcherj1@newpaltz.edu>
2023-03-24 13:38:36 -0400

    boiler plate code for server using express and created a server model and controller to link with client

--- server/index.js
+++ server/index.js
@@ -1,14 +1,35 @@
-const http = require('http');
+const express = require('express');
+const path = require('path');
+const jokes = require('./controllers/jokes');
+const products = require('./controllers/products');
+const app = express();
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000; //usually use this port for dev
+	const hostname = '127.0.0.1';
+	const port = process.env.PORT || 3000; //usually use this port for dev
 
-const server = http.createServer((req, res) => { //the fat arrow (=>) function
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Newpaltz from nodemon hello');
+// Middleware
+app
+	.use(express.json())
+	.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Actions
+app 
+	.get('/api/v1', (req, res) => {
+		res.send('Hello World! From Express')
+	})
+	.use('/api/v1/products', products)
+	.use('/api/v1/jokes', jokes);
+
+// Catch all
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/dist/index.html'))
 });
 
-server.listen(port, () => {
+console.log('1: About to start server');
+
+
+app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`); // backtik (`) allows you to place variables right in the string using ${variable} notation
 });
+
+console.log('3: Asked server to start');
